3929c1ee83b129eab5efc2b852dfa2f5111c938b
Drew Showalter <maelstroswim@gmail.com>
2018-04-07 22:19:02 +1000

    :recycle: clean up param parsing and use callback.

--- functions/index.js
+++ functions/index.js
@@ -10,25 +10,31 @@ exports.helloWorld = functions.https.onRequest((req, res) => {
   const userIds = ['U06TX7TQV', 'U2R30LACE'];
   if (!userIds.includes(req.body.user_id)) throw new Error('Only Drew is allowed to update the website, sorry!');
 
-  // FIXME: shit. process.exi() on our module is shitting up the return.
-  // remove process.exit in im-doin, and get this returning correctly
-  try {
-    updateStatus(
-      { message: message[0], background: message[1] },
-      {
-        apiKey: functions.config().imdoin.key,
-        authDomain: `${functions.config().imdoin.db}.firebaseapp.com`,
-        databaseURL: `https://${functions.config().imdoin.db}.firebaseio.com`,
-        projectId: `${functions.config().imdoin.db}`,
-        storageBucket: `${functions.config().imdoin.db}.appspot.com`,
-        messagingSenderId: functions.config().imdoin.sender,
-        password: functions.config().imdoin.password,
-        email: functions.config().imdoin.email
-      }
-    );
-  } catch (e) {
-    console.log('hm, an error', err);
-  }
+  var message = req.body.text.split(/\s-.{1}\s/ig);
+  const messageParams = { message: message[0] };
+
+  const backgroundParam = /\s-b{1}\s(\w*)/ig.exec(req.body.text);
+  if (backgroundParam) messageParams.background = backgroundParam[1];
+
+  const textParam = /\s-t{1}\s(\w*)/ig.exec(req.body.text);
+  if (textParam) messageParams.text = textParam[1];
+
+  // TODO: handle errors instead of just jamming on the saxaphone here.
+  // FIXME: longer process.exit wait, and implement async
+  updateStatus(
+    messageParams,
+    {
+      apiKey: functions.config().imdoin.key,
+      authDomain: `${functions.config().imdoin.db}.firebaseapp.com`,
+      databaseURL: `https://${functions.config().imdoin.db}.firebaseio.com`,
+      projectId: `${functions.config().imdoin.db}`,
+      storageBucket: `${functions.config().imdoin.db}.appspot.com`,
+      messagingSenderId: functions.config().imdoin.sender,
+      password: functions.config().imdoin.password,
+      email: functions.config().imdoin.email
+    },
+    () => setTimeout(() => process.exit(0), 10)
+  );
 
   return res.send({ text: 'Success... (probably) check <https://one19.codes>'})
 });
